dd39f0dc147af94ddb36580e3ceb3886a73b91b0
BriaBlunt <blunb635@newschool.edu>
2015-02-27 21:30:02 -0500

    Tut II

--- mypythonscript.py
+++ mypythonscript.py
@@ -1,11 +1,5 @@
 import rhinoscriptsyntax as rs
 from Rhino.Geometry import Point3d
-rs.AddCircle(Point3d.Origin, 5.00)
 
-print "hello world"
 
-intX = 10
-dblY = 5
-strName = "Bria"
-
-print(intX + dblY)
+rs.AddPoint
