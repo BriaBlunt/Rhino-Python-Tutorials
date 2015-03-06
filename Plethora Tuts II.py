b380b1fa4abe76e136eaf1682c2b48fe95fc7d2a
BriaBlunt <blunb635@newschool.edu>
2015-03-06 06:22:48 -0500

    Tut IX - Lists

--- Plethora Tuts II.py
+++ Plethora Tuts II.py
@@ -1,8 +1,31 @@
-import rhinoscriptsyntax as rs
-from LnBetCurvesClass import*
 
-strC1 = rs.GetObject("pick first curve", 4)
-strC2 = rs.GetObject("pick second curve", 4)
+#LISTS:
 
-obj01 = LnBetCurves (strC1, strC2, 60)
-obj01.drawLineBetweenCurves()
+myList01 = [54,22, "cat"]
+myList02 = [8, "cat", 33]
+myList03 = [64, 635, 12, 78]
+
+#myList.append(45)
+#myList01.extend(myList02)
+#myList01.remove(22)
+#myList01.pop(0)
+    #^This removes the first element "54"; (programming starts counting at 0)
+
+#x = myList01.pop(0)
+
+#print myList01
+#print x
+
+    ##how to find out how long your list is
+
+#print len(myList01)
+    ##output returns "3" to indicate that there are 3 elements in the list
+
+#print myList01.index("cat")
+    ##output = "2", tells the index of "cat"
+#print myList01.index(22)
+    ##vice versa - tells what the index is of 22 in myList01
+
+myList03.sort()
+print myList03
+    ##put in increasing order. The opposite is "myList03.reverse"
