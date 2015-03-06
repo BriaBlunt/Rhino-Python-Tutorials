ea7b1816672d9865273e41c038cb4a33cc6614b8
BriaBlunt <blunb635@newschool.edu>
2015-03-06 05:33:27 -0500

    Tut VII - Curve Class E.g.

--- mypythonscript.py
+++ mypythonscript.py
@@ -1,19 +1,27 @@
 import rhinoscriptsyntax as rs
-from math import*
+
 
 class myClass:
 
-    def __init__(self, _name):
-        self.name = _name
+    def __init__(self, _strCurve1, _strCurve2, _numOfDivs):
+        self.strCurve1 = _strCurve1
+        self.strCurve2 = _strCurve2
+        self.numOfDivs = _numOfDivs
 
-    def printName(self):
-        print(self.name)
+    def drawLineBetweenCurves(self):
+        ptList1 = rs.DivideCurve( self.strCurve1, self.numOfDivs, True, True)
+        ptList2 = rs.DivideCurve( self.strCurve2, self.numOfDivs, True, True)
 
-    def printAlert(self):
-        print("Alert!")
+        count = 0
+        for i in ptList1:
+            rs.AddLine( ptList1[count] , ptList2[count]  )
+            count += 1
 
-obj1 = myClass("Ana")
-obj1.printName()
+        print ("everything is okay so far")
 
-obj2 = myClass("Joe")
-obj2.printName()
+
+strC1 = rs.GetObject("pick first curve", 4)
+strC2 = rs.GetObject("pick second curve", 4)
+
+obj01 = myClass (strC1, strC2, 60)
+obj01.drawLineBetweenCurves()
